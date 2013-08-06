8621d576046c90d5a5cb75c53ec03d51c61a3530
daimatz <dai@daimatz.net>
2013-08-06 10:58:10 +0900

    split to .zprofile

---
+++ dot.zprofile
@@ -0,0 +1,54 @@
+# /usr/local などと同様に認識させたいディレクトリ
+function make_path() {
+    LOCAL_DIR=("$HOME/local" "$HOME/brew" "$HOME/dotfiles")
+    str=""
+    for i in $LOCAL_DIR; do
+        [[ -d "$i/$1" ]] && str="$str$i/$1:"
+    done
+    echo $str
+}
+
+export HISTFILE=~/.zsh_history
+export HISTSIZE=1000000
+export SAVEHIST=1000000
+export WORDCHARS='*?[]~=&;!#$%^(){}<>'
+
+export LSCOLORS=ExFxCxdxBxegedabagacad
+export LS_COLORS='di=01;36:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+export LSOPTION='-F --color=auto --si --show-control-char'
+export PATH=`make_path bin`$PATH
+export EDITOR=vim
+export LANG=ja_JP.UTF-8
+export PAGER=less
+
+export _Z_CMD=j
+export _Z_NO_RESOLVE_SYMLINKS=1
+
+# for heroku
+# Download Heroku Toolbelt standalone
+export PATH=$PATH:/usr/local/heroku/bin
+
+# for Ruby, migemo
+# eval "$(rbenv init -)" is too late
+export PATH=$HOME/.rbenv/shims:$PATH
+# rubygems
+RUBY_VERSION=$(ruby -e 'print RUBY_VERSION.gsub(/(\d)\.(\d)\.(\d)/, "\\1.\\2")')
+export GEM_HOME=$HOME/.gems/$RUBY_VERSION
+export PATH=$GEM_HOME/bin:$PATH
+
+# for Perl
+export PERL5LIB=$HOME/local
+
+# for Haskell
+export PATH=$HOME/.cabal/bin:$PATH
+export GHC_VERSION=`ghc --version 2> /dev/null | awk '{ print $8 }'`
+
+# for D
+export PATH=$HOME/.denv/shims:$HOME/.denv/bin:$PATH
+
+[[ ! -d /tmp/trash ]] && mkdir /tmp/trash
+[[ ! -d /tmp/undo ]] && mkdir /tmp/undo
+
+## 色設定
+TERM=xterm-256color
+# LANG=C perl ~/.zsh/256colors2.pl > /dev/null
--- dot.zsh/init.zsh
+++ dot.zsh/init.zsh
@@ -2,65 +2,6 @@ function err() {
     echo $* > /dev/stderr
 }
 
-# /usr/local などと同様に認識させたいディレクトリ
-function make_path() {
-    LOCAL_DIR=("$HOME/local" "$HOME/brew" "$HOME/dotfiles")
-    str=""
-    for i in $LOCAL_DIR; do
-        [[ -d "$i/$1" ]] && str="$str$i/$1:"
-    done
-    echo $str
-}
-
-# eval once
-if [ "$TERM" != "xterm-256color" ]; then
-    export HISTFILE=~/.zsh_history
-    export HISTSIZE=1000000
-    export SAVEHIST=1000000
-    export WORDCHARS='*?[]~=&;!#$%^(){}<>'
-
-    export LSCOLORS=ExFxCxdxBxegedabagacad
-    export LS_COLORS='di=01;36:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
-    export LSOPTION='-F --color=auto --si --show-control-char'
-    export PATH=`make_path bin`$PATH
-    export EDITOR=vim
-    export LANG=ja_JP.UTF-8
-    export PAGER=less
-
-    export _Z_CMD=j
-    export _Z_NO_RESOLVE_SYMLINKS=1
-
-    # for heroku
-    # Download Heroku Toolbelt standalone
-    export PATH=$PATH:/usr/local/heroku/bin
-
-    # for Ruby, migemo
-    # eval "$(rbenv init -)" is too late
-    export PATH=$HOME/.rbenv/shims:$PATH
-    RUBY_VERSION=1.9
-    export RUBYLIB=$HOME/local/lib/ruby/$RUBY_VERSION
-    # rubygems
-    export GEM_HOME=$HOME/.gem/$RUBY_VERSION
-    export PATH=$GEM_HOME/bin:$PATH
-
-    # for Perl
-    export PERL5LIB=$HOME/local
-
-    # for Haskell
-    export PATH=$HOME/.cabal/bin:$PATH
-    export GHC_VERSION=`ghc --version 2> /dev/null | awk '{ print $8 }'`
-
-    # for D
-    export PATH=$HOME/.denv/shims:$HOME/.denv/bin:$PATH
-
-    [[ ! -d /tmp/trash ]] && mkdir /tmp/trash
-    [[ ! -d /tmp/undo ]] && mkdir /tmp/undo
-
-    ## 色設定
-    TERM=xterm-256color
-    # LANG=C perl ~/.zsh/256colors2.pl > /dev/null
-fi
-
 # alias
 alias rm='mv -f --backup=numbered --target-directory /tmp/trash'
 alias here="open ."
@@ -165,7 +106,7 @@ function g() {
     echo $out
 }
 # cabal-dev install <executable package>
-# cabal-dev でインストールしたあと、実行バイナリを $HOME/local/bin に simlink
+# cabal-dev でインストールしたあと、実行バイナリを $HOME/local/bin に symlink
 function cabal-dev-install-executable() {
     local package_path=$HOME/local/cabal-dev
     local executable_path=$HOME/local/bin
