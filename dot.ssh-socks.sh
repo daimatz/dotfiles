93149c0798aee79cc3a24e103db5ee0537044bcd
daimatz <dai@daimatz.net>
2017-02-12 02:02:57 +0900

    gc, add ssh-socks

---
+++ dot.ssh-socks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+host=$1
+port=$2
+
+if [ "$host" = "" -o "$port" = "" ]; then
+    echo "Usage: $0 <host> <port>"
+    exit 1
+fi
+
+while :; do
+    if [ -z "$(ps aux | grep "ssh -f -N -D $port $host" | grep -v grep)" ]; then
+        echo "tunneling $host:$port ..."
+        ssh -f -N -D $port $host &> /dev/null
+    fi
+    sleep 5
+done
