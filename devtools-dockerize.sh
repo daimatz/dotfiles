7b339544141da6584d5d686b8855909b51c6d2c5
daimatz <dai@daimatz.net>
2019-03-21 22:25:15 +0900

    use both docker, docker-compose

--- devtools-dockerize.sh
+++ devtools-dockerize.sh
@@ -5,29 +5,47 @@
 bin=$HOME/bin
 mkdir -p $bin
 cp devtools $bin
+cp docker-compose.yml $bin
 
 function create() {
     path=$1
-    cmd=$2
+    docker=$2
+    dockerarg=$3
+    cmd=$4
+    echo "$@"
     echo "#!/bin/bash" > $path
-    echo "$HOME/bin/devtools $cmd \$*" >> $path
+    echo "$HOME/bin/devtools \"$docker\" \"$dockerarg\" $cmd \"\$@\"" >> $path
     chmod +x $path
 }
 
 for i in \
-    "ag -t -t '' -t" \
-    "global -t -t '' -t" \
-    "gtags -t -t '' -t" \
-    "jq -it -i -i -i" \
-    "nkf -it -i -i -i" \
-    "tig -it -it -it -it" \
-    "scala -it -it -it -it" \
-    "java -it -it -it -it" \
-    "javac -it -it -it -it" \
+    "docker ag -t -t '' -t" \
+    "docker global -t -t '' -t" \
+    "docker-compose gtags -t -t '' -t" \
+    "docker jq -it -i -i -i" \
+    "docker nkf -it -i -i -i" \
+    "docker tig -it -it -it -it" \
+    "docker scala -it -it -it -it" \
+    "docker java -it -it -it -it" \
+    "docker javac -it -it -it -it" \
     ; do
-    create $bin/$(echo $i | awk '{print $1}') "$i"
+    create \
+        $bin/$(echo "$i" | awk '{print $2}') \
+        $(echo "$i" | awk '{print $1}') \
+        '' \
+        "$(echo "$i" | cut -f 2- -d ' ')"
 done
 
-create $HOME/bin/go "'GOROOT=~/.goroot GOPATH=/host$HOME GOOS=darwin GOARCH=amd64 ~/.goroot/bin/go' -it -it -it -it"
-create $HOME/bin/gorun "'GOROOT=~/.goroot GOPATH=/host$HOME ~/.goroot/bin/go run' -it -it -it -it"
-create $HOME/bin/sbt "'SBT_OPTS=\"-Xms512M -Xmx8192M -Xss2M -XX:MaxMetaspaceSize=2048M\" java -Dsbt.ivy.home=/host$HOME/.ivy2 -Divy.home=/host$HOME/.ivy2 -jar /usr/share/sbt/bin/sbt-launch.jar' -it -it -it -it"
+# don't use /nfshost to avoid nfs-related issue
+create $HOME/bin/go \
+  "docker-compose" \
+  "-e GOROOT=/root/.goroot -e GOPATH=/host$HOME -e GOOS=darwin -e GOARCH=amd64" \
+  "/root/.goroot/bin/go -it -it -it -it"
+create $HOME/bin/gorun \
+  "docker" \
+  "-e GOROOT=/root/.goroot -e GOPATH=/host$HOME" \
+  "'/root/.goroot/bin/go run' -it -it -it -it"
+create $HOME/bin/sbt \
+  "docker-compose" \
+  "" \
+  "'java -Dsbt.ivy.home=/host$HOME/.ivy2 -Divy.home=/host$HOME/.ivy2 -jar /usr/share/sbt/bin/sbt-launch.jar' -it -it -it -it"
