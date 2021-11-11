40f7fdc6a16a8799b81836b05a009481e368e905
daimatz <dai@daimatz.net>
2021-11-11 23:52:57 +0900

    remove code-server

--- linker.sh
+++ linker.sh
@@ -19,21 +19,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +PlugClean! +qall
 (cd ~/.vim/plugged/vimproc && make)
-
-mkdir -p $HOME/.local/share/code-server/User
-ln -sf $DOTFILES/vscode/*.json $HOME/.local/share/code-server/User/
-# code-server --list-extensions
-cat << EOF | while read i; do code-server --install-extension $i; done
-    daltonjorge.scala
-    golang.go
-    ms-vscode.cpptools
-    redhat.java
-    scala-lang.scala
-    scalameta.metals
-    vscjava.vscode-java-debug
-    vscjava.vscode-java-dependency
-    vscjava.vscode-java-pack
-    vscjava.vscode-java-test
-    vscjava.vscode-maven
-    vscodevim.vim
-EOF
